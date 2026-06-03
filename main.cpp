8172f0ed1a17d30598d13c78ce6cb23f30f59b7c
FlerkenArtem <vokak738@yandex.ru>
2026-06-03 13:42:17 +0300

    Вынос проверки ошибок приема в функцию

--- main.cpp
+++ main.cpp
@@ -50,6 +50,9 @@ unsigned short calculateChecksum(unsigned short *buffer, int size);
 /// Оператор сравнения 2 GUID
 bool operator<(const GUID &guid1, const GUID &guid2);
 
+/// Обработка ошибок ICMP
+void errors(unsigned char type, unsigned char code);
+
 int main()
 {
     system("chcp 65001 > nul");
@@ -281,83 +284,10 @@ unsigned long long ping(sockaddr_in destAddr, int steps)
 
             icmpPacket *recvPack = (icmpPacket *) (recvBuffer + ipHeaderLen);
 
-            // Типы кроме 0 пропускаются, выводится ошибка
+            // Типы кроме 0 пропускаются
             if (recvPack->header.type != 0) {
-                if (recvPack->header.type == 3) {
-                    cerr << "Ошибка: Адресат недостижим.\t";
-                    if (recvPack->header.code == 0) {
-                        cerr << "Сеть недоступна.";
-                    } else if (recvPack->header.code == 1) {
-                        cerr << "Узел недоступен.";
-                    } else if (recvPack->header.code == 2) {
-                        cerr << "Протокол недоступен.";
-                    } else if (recvPack->header.code == 3) {
-                        cerr << "Порт недоступен.";
-                    } else if (recvPack->header.code == 4) {
-                        cerr << "Необходима фрагментация, но не задан бит ее запрета.";
-                    } else if (recvPack->header.code == 5) {
-                        cerr << "Ошибка на исходном маршруте.";
-                    } else if (recvPack->header.code == 6) {
-                        cerr << "Сеть адресата неизвестна.";
-                    } else if (recvPack->header.code == 7) {
-                        cerr << "Узел адресата неизвестен.";
-                    } else if (recvPack->header.code == 8) {
-                        cerr << "Исходный узел изолирован.";
-                    } else if (recvPack->header.code == 9) {
-                        cerr << "Сеть адресата административно изолирована.";
-                    } else if (recvPack->header.code == 10) {
-                        cerr << "Узел адресата административно изолирован.";
-                    } else if (recvPack->header.code == 11) {
-                        cerr << "Сеть недоступна для TOS.";
-                    } else if (recvPack->header.code == 12) {
-                        cerr << "Узел недоступен для TOS.";
-                    } else if (recvPack->header.code == 13) {
-                        cerr << "Связь административно запрещена фильтрацией.";
-                    } else if (recvPack->header.code == 14) {
-                        cerr << "Нарушение приоритета узлов.";
-                    } else if (recvPack->header.code == 15) {
-                        cerr << "Пренебрежение приоритетом узлов.";
-                    } else {
-                        cerr << "Ошибка. Код: " << recvPack->header.code;
-                    }
-                } else if (recvPack->header.type == 4 && recvPack->header.code == 0) {
-                    cerr << "Ошибка.\tПодавление отправителя.";
-                } else if (recvPack->header.type == 5) {
-                    cerr << "Ошибка: Перенаправление.\t";
-                    if (recvPack->header.code == 0) {
-                        cerr << "Перенаправление для сети.";
-                    } else if (recvPack->header.code == 1) {
-                        cerr << "Перенаправление на узел.";
-                    } else if (recvPack->header.code == 2) {
-                        cerr << "Перенаправление на TOS и сеть.";
-                    } else if (recvPack->header.code == 3) {
-                        cerr << "Перенаправление на TOS и узел.";
-                    } else {
-                        cerr << "Ошибка. Код: " << recvPack->header.code;
-                    }
-                } else if (recvPack->header.type == 11) {
-                    cerr << "Ошибка: Время превышено.\t";
-                    if (recvPack->header.code == 0) {
-                        cerr << "TTL в ходе транзита равен 0.";
-                    } else if (recvPack->header.code == 1) {
-                        cerr << "TTL в ходе повторной сборки равен 0.";
-                    } else {
-                        cerr << "Ошибка. Код: " << recvPack->header.code;
-                    }
-                } else if (recvPack->header.type == 12) {
-                    cerr << "Ошибка: Проблема параметра.\t";
-                    if (recvPack->header.code == 0) {
-                        cerr << "Неверный заголовок IP.";
-                    } else if (recvPack->header.code == 1) {
-                        cerr << "Отсутствует требуемый параметр.";
-                    } else {
-                        cerr << "Ошибка. Код: " << recvPack->header.code;
-                    }
-                } else {
-                    cerr << "Ошибка. Тип: " << recvPack->header.type
-                         << ", код: " << recvPack->header.code;
-                }
-
+                // Вывод ошибок
+                errors(recvPack->header.type, recvPack->header.code);
                 continue;
             }
 
@@ -470,4 +400,83 @@ bool operator<(const GUID &guid1, const GUID &guid2)
         }
     }
     return false;
+}
+
+void errors(unsigned char type, unsigned char code)
+{
+    if (type == 0 && code == 0)
+        return;
+    if (type == 3) {
+        cerr << "Ошибка: Адресат недостижим.\t";
+        if (code == 0) {
+            cerr << "Сеть недоступна.";
+        } else if (code == 1) {
+            cerr << "Узел недоступен.";
+        } else if (code == 2) {
+            cerr << "Протокол недоступен.";
+        } else if (code == 3) {
+            cerr << "Порт недоступен.";
+        } else if (code == 4) {
+            cerr << "Необходима фрагментация, но не задан бит ее запрета.";
+        } else if (code == 5) {
+            cerr << "Ошибка на исходном маршруте.";
+        } else if (code == 6) {
+            cerr << "Сеть адресата неизвестна.";
+        } else if (code == 7) {
+            cerr << "Узел адресата неизвестен.";
+        } else if (code == 8) {
+            cerr << "Исходный узел изолирован.";
+        } else if (code == 9) {
+            cerr << "Сеть адресата административно изолирована.";
+        } else if (code == 10) {
+            cerr << "Узел адресата административно изолирован.";
+        } else if (code == 11) {
+            cerr << "Сеть недоступна для TOS.";
+        } else if (code == 12) {
+            cerr << "Узел недоступен для TOS.";
+        } else if (code == 13) {
+            cerr << "Связь административно запрещена фильтрацией.";
+        } else if (code == 14) {
+            cerr << "Нарушение приоритета узлов.";
+        } else if (code == 15) {
+            cerr << "Пренебрежение приоритетом узлов.";
+        } else {
+            cerr << "Ошибка. Код: " << code;
+        }
+    } else if (type == 4 && code == 0) {
+        cerr << "Ошибка.\tПодавление отправителя.";
+    } else if (type == 5) {
+        cerr << "Ошибка: Перенаправление.\t";
+        if (code == 0) {
+            cerr << "Перенаправление для сети.";
+        } else if (code == 1) {
+            cerr << "Перенаправление на узел.";
+        } else if (code == 2) {
+            cerr << "Перенаправление на TOS и сеть.";
+        } else if (code == 3) {
+            cerr << "Перенаправление на TOS и узел.";
+        } else {
+            cerr << "Ошибка. Код: " << code;
+        }
+    } else if (type == 11) {
+        cerr << "Ошибка: Время превышено.\t";
+        if (code == 0) {
+            cerr << "TTL в ходе транзита равен 0.";
+        } else if (code == 1) {
+            cerr << "TTL в ходе повторной сборки равен 0.";
+        } else {
+            cerr << "Ошибка. Код: " << code;
+        }
+    } else if (type == 12) {
+        cerr << "Ошибка: Проблема параметра.\t";
+        if (code == 0) {
+            cerr << "Неверный заголовок IP.";
+        } else if (code == 1) {
+            cerr << "Отсутствует требуемый параметр.";
+        } else {
+            cerr << "Ошибка. Код: " << code;
+        }
+    } else {
+        cerr << "Ошибка. Тип: " << type << ", код: " << code;
+    }
 }
